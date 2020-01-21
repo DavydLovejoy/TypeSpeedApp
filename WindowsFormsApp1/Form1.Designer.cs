7f5de16a13e47c485a1360aa7133c298e7699427
Dids Lovejoy <Davydlovejoy@gmail.com>
2020-01-21 15:12:34 +0100

    Ajoutez des fichiers projet.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(300, 133);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(188, 65);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "UiTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<Label> lstFinal = new List<Label>();
+        private Random random = new Random();
+        bool clicked = false;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            List<String> lstNames = new List<String>()
+            {
+                "First Name","Last Name","Email Address","Phone Number"
+            };
+            Dictionary<Label, TextBox> dictAssets = new Dictionary<Label, TextBox>();
+
+
+            Controls.Remove(sender as Button);
+            this.SuspendLayout();
+            dictAssets = beginGame(lstNames);
+
+            Label instr = new Label();
+            instr.Text = "Press Next when you have typed the result.";
+            instr.Location = new Point(325, 10);
+            instr.AutoSize = true;
+            Controls.Add(instr);
+            Button next = new Button();
+            Button fin = new Button();
+            next.Text = "Next?";
+            fin.Text = "Finished?";
+            fin.Location = new Point(400, 350);
+            next.Click += new System.EventHandler(click_btnNext);
+            next.Location = new Point(300, 350);
+            Controls.Add(next);
+            Controls.Add(fin);
+
+
+            foreach (Label lbl in dictAssets.Keys)
+            {
+                Controls.Add(dictAssets[lbl]);
+                Controls.Add(lbl);
+                lbl.Invalidate();
+                lbl.Update();            
+                next.Invalidate();
+                next.Update();
+
+                this.ResumeLayout(false);
+            }
+            
+        }
+
+        private Dictionary<Label, TextBox> beginGame(List<String> lstItems)
+        {
+            List<String> lstFinal = new List<String>();
+            Dictionary<Label, TextBox> dictAssets = new Dictionary<Label, TextBox>();
+
+               int x = 0;
+            int size = lstItems.Count;
+            while (x < size)
+            {
+                int rndNum = random.Next(0, lstItems.Count - 1);
+                lstFinal.Add(lstItems[rndNum]);
+                lstItems.RemoveAt(rndNum);
+                x = x + 1;
+            }
+
+            foreach(String text in lstFinal)
+            {
+                TextBox txt1 = new TextBox();
+                Label info = new Label();
+                int posX = random.Next(40, 700);
+                int posY = random.Next(50, 400);
+
+                txt1.Name = text;
+                txt1.Location = new Point(posX, posY);
+                info.Text = text;
+                info.Location = new Point(posX, posY - 20);
+                dictAssets.Add(info, txt1);
+            }
+            
+        return dictAssets;
+        }
+        private void click_btnNext(object sender, EventArgs e)
+        {
+            clicked = true;
+            List<string> lstResponse = new List<string>();
+            var lstBox = GetAllControls(Form.ActiveForm).OfType<TextBox>();
+            var lstLbl = GetAllControls(Form.ActiveForm).OfType<Label>();
+            foreach(TextBox line in lstBox)
+            {
+                lstResponse.Add(String.Format("{0}|{1}", line.Text, line.Name));
+                Controls.Remove(line);
+            }
+            foreach(Label label in lstLbl)
+            {
+                Controls.Remove(label);
+            }
+
+            foreach(string answer in lstResponse)
+            {
+                string boxName = answer.Remove(0, answer.IndexOf('|') + 1 );
+                
+
+            }
+        }
+
+        public static IEnumerable<Control> GetAllControls(Control root)
+        {
+            var stack = new Stack<Control>();
+            stack.Push(root);
+
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                foreach (Control child in next.Controls)
+                    stack.Push(child);
+
+                yield return next;
+            }
+        }
+
+    }
+}
